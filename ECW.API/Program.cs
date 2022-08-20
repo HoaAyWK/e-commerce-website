4491eb0835fc688af406f24ae24e0bd1b8d5ac24
HoaAyWK <vycoi1511@gmail.com>
2022-08-20 14:51:13 +0700

    Init migration DbContext

--- ECW.API/Program.cs
+++ ECW.API/Program.cs
@@ -1,6 +1,43 @@
+using System.Text;
+using ECW.ApplicationCore;
+using ECW.Infrastructure;
+using ECW.Infrastructure.Data;
+using ECW.Infrastructure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+
+// Add DbContext
+Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig:Secret"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +45,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+    try
+    {
+        var productContext = scopedProvider.GetRequiredService<ProductContext>();
+        await ProductContextSeed.SeedAsync(productContext, app.Logger);
+
+        var userManager = scopedProvider.GetRequiredService<UserManager<AppUser>>();
+        var roleManager = scopedProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var identityContext = scopedProvider.GetRequiredService<AppIdentityDbContext>();
+        await AppIdentityDbContextSeed.SeedAsync(identityContext, userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
